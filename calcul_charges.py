e7a8d67a8df2de9f0726931c77b716dc277bebfc
lvlagicpeter <pierre.beylard@hotmail.fr>
2021-03-17 17:01:19 +0100

    début création des classes factures et releves

---
+++ calcul_charges.py
@@ -0,0 +1,37 @@
+import math
+
+
+class Invoice(object):
+	"""docstring for ClassName"""
+	def __init__(self, arg):
+		super(Invoice, self).__init__()
+		self.arg = arg
+
+class meter_reading(object):
+	"""docstring for ClassName"""
+	def __init__(self, arg):
+		super(meter_reading, self).__init__()
+		self.arg = arg
+		
+
+def water ():
+	pass
+
+def waste_taxe():
+	pass
+
+def electricity ():
+	pass
+
+def building_cleaning():
+	pass
+
+def total_charges ():
+	pass
+
+def charges_per_premise():
+	pass
+
+def calcul_annee_releve():
+#will allow to determine the year of the meter reading based on meter reading date 
+	pass
